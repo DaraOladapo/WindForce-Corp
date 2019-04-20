453624b81f94a2db2767c70a4788e92683a7e462
Dara Oladapo <daraoladapo@outlook.com>
2019-04-20 06:34:59 -0400

    NUGET update

--- WindForceCorp/WindForceCorp/ViewModels/EmployeeData.cs
+++ WindForceCorp/WindForceCorp/ViewModels/EmployeeData.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -13,35 +14,12 @@ namespace WindForceCorp.ViewModels
     public class EmployeeData
     {
         public List<Employee> allEmployeeData = new List<Employee>();
-        public async Task<List<Employee>> GetAllEmployeeDataAsync()
+        public async Task<ObservableCollection<Employee>> GetAllEmployeeDataAsync()
         {
-         
-            Employee employee = new Employee();
-            using (var client = new HttpClient())
+            return new ObservableCollection<Employee>
             {
-                try
-                {
-                    client.BaseAddress = new Uri("http://windforce-corp.azurewebsites.net/api/employees");
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                    var response = await client.GetAsync(client.BaseAddress);
-
-                    response.EnsureSuccessStatusCode();
-
-                    var stringResult = await response.Content.ReadAsStringAsync();
-                    allEmployeeData= JsonConvert.DeserializeObject<List<Employee>>(stringResult);
-
-
-
-                }
-                catch (HttpRequestException httpRequestException)
-                {
-                    var error = $"Error getting employee data: {httpRequestException.Message}";
-                }
-                return allEmployeeData;
-
-            }
+            };
         }
     }
 }
